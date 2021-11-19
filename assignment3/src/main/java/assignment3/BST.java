71f333e9a0ee505bb8f82db64aec09e47922b37b
Shoaib Asghar <i190406@nu.edu.pk>
2021-11-20 02:28:02 +0500

    3rd time

--- assignment3/src/main/java/assignment3/BST.java
+++ assignment3/src/main/java/assignment3/BST.java
@@ -5,6 +5,8 @@ public class BST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Choose any option from the Following ");
+		System.out.println("Enter 5: For Exiting!  ");
+		System.out.println("Enter 5: For Exiting!  ");
 	}
 
 }
