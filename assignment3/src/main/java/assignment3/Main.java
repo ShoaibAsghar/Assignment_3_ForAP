4bd5891125aa8028aa26a370742040ae4fe177fb
Shoaib Asghar <i190406@nu.edu.pk>
2021-11-20 02:16:44 +0500

    2nd attempt

---
+++ assignment3/src/main/java/assignment3/Main.java
@@ -0,0 +1,19 @@
+package assignment3;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Choose any option from the Following ");
+		System.out.println("Enter 1: For Displaying BST build from Vocabulary File ");
+		System.out.println("Enter 2: For Displaying Vector build from Input File ");
+		System.out.println("Enter 3: For Viewing Match Words and its Frequency ");
+		System.out.println("Enter 4: For Searching a query - It should Display all the Files query found ");
+		System.out.println("Enter 5: For Exiting!  ");
+		
+
+	}
+
+}
