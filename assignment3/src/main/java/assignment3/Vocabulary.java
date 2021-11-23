bde1409d2b28804db53e12dc3dbfa13f34227045
Shoaib Asghar <i190406@nu.edu.pk>
2021-11-23 20:03:15 +0500

    final

---
+++ assignment3/src/main/java/assignment3/Vocabulary.java
@@ -0,0 +1,15 @@
+package assignment3;
+import java.util.*;
+
+public class Vocabulary implements Runnable
+{
+	Thread t;
+	public void run()
+	  {
+	    System.out.println("Displaying BST : ");	  
+	  
+	  
+	  }
+	
+
+}
